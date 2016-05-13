7779831c68f12d8c2e951316323741e7697c3a06
Sam Rapaport <sam@gigasavvy.com>
2016-05-13 13:43:55 -0700

    Implement \ArrayAccess on YardiLead.

--- src/YardiLead.php
+++ src/YardiLead.php
@@ -9,8 +9,9 @@
 namespace YardiClient;
 
 use DateTime;
+use ArrayAccess;
 
-class YardiLead
+class YardiLead implements ArrayAccess
 {
     protected $attributes;
     public $id;
@@ -224,4 +225,34 @@ class YardiLead
     {
         unset($this->attributes[$name]);
     }
+
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->attributes[] = $value;
+        } else {
+            $this->attributes[$offset] = $value;
+        }
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->attributes[$offset]);
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->attributes[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        return isset($this->attributes[$offset]) ? $this->attributes[$offset] : null;
+    }
 }
+
+$a = new YardiLead;
+$a['foo'] = 'bar';
+
+echo $a['foo'];
+echo 'yess';
